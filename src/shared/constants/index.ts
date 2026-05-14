35961698c637f3d114f083103499e61bc7994820
jy05280731 <2811618503@qq.com>
2026-05-15 03:27:47 +0800

    Update index.ts

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,5 +1,14 @@
-/**
- * 共享常量导出
- */
+// 网站默认视频源，所有用户打开都会自动加载
+export const DEFAULT_VIDEO_SOURCES = [
+  {"name":"高清主源1-阿冰资源","url":"https://api.apibdzy.com/api.php/provide/vod/","isEnabled":true},
+  {"name":"高清主源2-福利资源","url":"https://cj.lziapi.com/api.php/provide/vod/","isEnabled":true},
+  {"name":"高清主源3-闪电资源","url":"https://sdzyapi.com/api.php/provide/vod/","isEnabled":true},
+  {"name":"高清主源4-魔都资源","url":"https://www.mdzyapi.com/api.php/provide/vod/","isEnabled":true},
+  {"name":"备用源1-小盒子主仓","url":"https://xhztv.top/xhz","isEnabled":true},
+  {"name":"备用源2-小盒子4K仓","url":"https://xhztv.top/4k.json","isEnabled":true},
+  {"name":"备用源3-暴风资源","url":"https://bfzyapi.com/api.php/provide/vod/","isEnabled":true},
+  {"name":"备用源4-高天流云镜像","url":"https://raw.githubusercontent.com/gaotianliuyun/gao/master/js.json","isEnabled":true}
+];
 
-export * from './countries'
+// 其他常量保持不变（如果有）
+export * from './countries';
